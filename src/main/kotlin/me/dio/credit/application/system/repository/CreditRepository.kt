b5e97bcf0f9142f681650ed9121b63c9df8c60e1
wilson-silva <wilson-gs@hotmail.com>
2023-05-25 19:59:29 -0300

    update application.yml and query credit repository

--- src/main/kotlin/me/dio/credit/application/system/repository/CreditRepository.kt
+++ src/main/kotlin/me/dio/credit/application/system/repository/CreditRepository.kt
@@ -10,6 +10,6 @@ import java.util.*
 interface CreditRepository : JpaRepository<Credit, Long>{
     fun findByCreditCode(creditCode: UUID): Credit?
 
-    @Query(value = "SELECT * FROM CREDIT WHERE CUSTOMER_ID = ?1", nativeQuery = true)
+    @Query(value = "SELECT * FROM CREDITO WHERE CUSTOMER_ID = ?1", nativeQuery = true)
     fun findAllByCustomerId(customerId: Long): List<Credit>
 }
