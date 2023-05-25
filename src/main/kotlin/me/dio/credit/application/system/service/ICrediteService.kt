d8c2adbc2068e862fb904227020f463673ee0c85
wilson-silva <wilson-gs@hotmail.com>
2023-05-25 14:39:38 -0300

    created service classes

---
+++ src/main/kotlin/me/dio/credit/application/system/service/ICrediteService.kt
@@ -0,0 +1,13 @@
+package me.dio.credit.application.system.service
+
+import me.dio.credit.application.system.entity.Credit
+import java.util.UUID
+
+interface ICrediteService {
+
+    fun save(credit: Credit): Credit
+
+    fun findAllByCustomer(customerId: Long): List<Credit>
+
+    fun findByCreditCode(customerId: Long, creditCode: UUID): Credit
+}
